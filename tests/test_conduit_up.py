6f4356befee57c69e7968833532908c286cf1ae5
Jozsa Tamas <84121041+plresearch999@users.noreply.github.com>
2021-07-20 10:01:47 +0200

    Create test_conduit_up.py

---
+++ tests/test_conduit_up.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.chrome.options import Options
+
+
+# In order for ChromeDriverManager to work you must pip install it in your own environment.
+browser_options = Options()
+browser_options.headless = True
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
+
+def test_conduit_up():
+    driver.get("http://localhost:1667")
+    time.sleep(5)
+    driver.save_screenshot("out/screenshot.png")
+    driver.quit()
