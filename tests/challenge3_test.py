cf7098eb54ec1a0d3dd3e3db4048522590825e81
Jozsa Tamas <84121041+plresearch999@users.noreply.github.com>
2021-07-20 10:01:05 +0200

    Update challenge3_test.py

--- tests/challenge3_test.py
+++ tests/challenge3_test.py
@@ -4,14 +4,16 @@
 #
 # https://react-card-2a6c5.web.app/
 
-
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+from selenium.webdriver.chrome.options import Options
+
 
 # In order for ChromeDriverManager to work you must pip install it in your own environment.
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
+browser_options = Options()
+browser_options.headless = True
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
 
 def test_webshop():
     driver.set_window_size(3000, 3000)
