cbb7acb90ce75571d62f5f30fa621e28fba2afb3
Jozsa Tamas <84121041+plresearch999@users.noreply.github.com>
2021-07-26 15:00:50 +0200

    Update test_conduit_up.py

--- tests/test_conduit_up.py
+++ tests/test_conduit_up.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 from selenium.webdriver.chrome.options import Options
-
+import pathlib
 
 # In order for ChromeDriverManager to work you must pip install it in your own environment.
 browser_options = Options()
@@ -14,3 +14,9 @@ def test_conduit_up():
     time.sleep(5)
     driver.save_screenshot("screenshot.png")
     driver.quit()
+    
+    with open(pathlib.path("test.txt"), "w") as t:
+        t.write('hello')
+        
+    with open(pathlib.path("test.txt"), "rt") as t:
+        print(t.read())
